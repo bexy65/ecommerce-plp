7e83b0b3c635b4af61f4d1cf65632d9f4df492ef
bexy65 <naimertan65@gmail.com>
2023-08-01 11:28:16 +0300

    fetching logic from database

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 let productGrid = document.getElementById('productGrid');
+let navbarList = document.querySelector('.navbar-nav');
 
-function loadProducts() {
-  productGrid.innerHTML = `
-    <div class="card">
-        <img src="images/4.svg" alt="" width="50" height="50" class="d-inline-block align-text-top">
-        <h5 class="card-title"></h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  `;
-}
-
-loadProducts();
-
-async function fetchProducts() {
+const fetchData = async (url) => {
   try {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const products = await response.json();
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error(error);
   }
+};
+
+const categoriesFetch = fetchData('https://api.escuelajs.co/api/v1/categories');
+const productsFetch = fetchData('https://api.escuelajs.co/api/v1/products');
+
+categoriesFetch.then((categories) => {
+  for (let i = 0; i < categories.length; i++) {
+    loadCategories();
+  }
+});
+
+productsFetch.then((products) => {
+  for (let i = 0; i < products.length; i++) {
+    loadProducts(products[i].title, products[i].price, products[i].image);
+  }
+});
+
+function loadCategories() {
+  navbarList.innerHTML += `
+    <li class="nav-item">
+      <a class="nav-link" href="/${categories[i].name}">${categories[i].name}</a>
+    </li>
+  `;
 }
 
-fetchProducts();
+function loadProducts(title, price, image) {
+  productGrid.innerHTML += `
+    <div class="card">
+        <img src="${image}" alt="" width="50" height="50" class="d-inline-block align-text-top">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${price}</p>
+        <a href="#" class="btn btn-primary">ADD</a>
+    </div>
+  `;
+}
